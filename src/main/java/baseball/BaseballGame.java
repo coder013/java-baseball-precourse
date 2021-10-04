cba12d61a74d39d6723982b681569a367a1b4101
coder013 <wnssh607@gmail.com>
2021-10-04 23:26:37 +0900

    랜덤 값 생성 기능 수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,22 +2,43 @@ package baseball;
 
 import nextstep.utils.Randoms;
 
-public class BaseballGame {
+import java.util.ArrayList;
+import java.util.List;
 
-    private final static int minValue = 123;
-    private final static int maxValue = 987;
+public class BaseballGame {
+    private final static int startInclusive = 1;
+    private final static int endInclusive = 9;
 
     public void playBaseballGame() {
         try {
-            int randomValue = getRandomValue();
+            List<Integer> randomValueList = getRandomValueList();
 
-            System.out.println("랜덤 값 : " + randomValue);
+            System.out.println("랜덤 값 : " + randomValueList.get(0) + randomValueList.get(1) + randomValueList.get(2));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public int getRandomValue() {
-        return Randoms.pickNumberInRange(minValue, maxValue);
+    private List<Integer> getRandomValueList() {
+        List<Integer> randomValueList = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            randomValueList.add(getRandomValue(randomValueList));
+        }
+
+        return randomValueList;
     }
+    // 서로 다른 수로 이루어진 3자리 수 생성
+
+    private int getRandomValue(List<Integer> list) {
+        int randomValue = getRandomValue();
+
+        return list.contains(randomValue) ? getRandomValue(list) : randomValue;
+    }
+    // 재귀함수, 중복된 수일 경우 다시 실행
+
+    private int getRandomValue() {
+        return Randoms.pickNumberInRange(startInclusive, endInclusive);
+    }
+    // 1~9 사이의 숫자를 가져온다.
 }
