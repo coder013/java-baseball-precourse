c89e64aced10107aa3c194370b5b3ad5200c5b67
coder013 <wnssh607@gmail.com>
2021-10-05 00:33:33 +0900

    숫자 입력 기능 개발

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import nextstep.utils.Console;
 import nextstep.utils.Randoms;
 
 import java.util.ArrayList;
@@ -13,7 +14,9 @@ public class BaseballGame {
         try {
             List<Integer> randomValueList = getRandomValueList();
 
-            System.out.println("랜덤 값 : " + randomValueList.get(0) + randomValueList.get(1) + randomValueList.get(2));
+            System.out.printf("%d ~ %d의 서로 다른 수로 이루어진 3자리 숫자를 맞추는 게임입니다.\n", startInclusive, endInclusive);
+
+            int inputValue = getInputValueByInt();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -35,10 +38,39 @@ public class BaseballGame {
 
         return list.contains(randomValue) ? getRandomValue(list) : randomValue;
     }
-    // 재귀함수, 중복된 수일 경우 다시 실행
+    // 재귀함수, 가져온 숫자가 중복된 숫자일 경우 다시 실행
 
     private int getRandomValue() {
         return Randoms.pickNumberInRange(startInclusive, endInclusive);
     }
-    // 1~9 사이의 숫자를 가져온다.
+
+    private int getInputValueByInt() {
+        String inputValue = "";
+        boolean result = false;
+
+        while (!result) {
+            System.out.print("숫자를 입력하세요. : ");
+
+            inputValue = getInputValue();
+            result = checkValidation(inputValue);
+        }
+
+        return Integer.parseInt(inputValue);
+    }
+    // 사용자가 입력한 숫자를 가져온다.
+
+    private String getInputValue() {
+        return Console.readLine();
+    }
+
+    private boolean checkValidation(String inputValue) {
+        String pattern = "^[1-9]{3}$";
+
+        if (!inputValue.matches(pattern)) {
+            System.out.printf("유효하지 않은 숫자입니다. %d ~ %d의 수로 이루어진 3자리 숫자를 입력하세요.\n", startInclusive, endInclusive);
+            return false;
+        }
+
+        return true;
+    }
 }
